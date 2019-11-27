e05e39924037d3ef7a1684f85bad44d9564433c9
Michail12C <dagobertusdagobertus@gmail.com>
2019-11-27 15:21:08 +0200

    localstate

--- src/components/Profile/MyPosts/Profileinfo/Profileinfo.js
+++ src/components/Profile/MyPosts/Profileinfo/Profileinfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import c from'./Profileinfo.module.css';
 import Preloader from '../../../Common/Preloader/Preloader.js';
+import ProfileStatus from './ProfileStatus.js';
 
 
 const Profileinfo = (props) => {
@@ -11,12 +12,11 @@ if(!props.profile ){
 	return (
       <div>
       <div className = {c.example}>
-      <img width = '100%' height = '300' src="https://www.vegascreativesoftware.com/fileadmin/user_upload/products/vegas_post/1/vegas_image/vegas-image-dehaze-a-int.jpg"/>
+      {/*<img width = '100%' height = '300' src="https://www.vegascreativesoftware.com/fileadmin/user_upload/products/vegas_post/1/vegas_image/vegas-image-dehaze-a-int.jpg"/>*/}
       </div>
     <div className = {c.descriptionBlock}>
     <img src = {props.profile.photos.large}/>
-    <div>{props.profile.fullName}</div>
-    <div>{props.profile.aboutMe}</div>
+    <ProfileStatus status={'hello my friends'}/>
     </div>
    </div>
   )
